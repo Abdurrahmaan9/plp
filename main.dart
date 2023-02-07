5e1bf6da68a39611ceded62e256c1895e3515400
abdur-rahmaan chimalo <>
2023-02-07 16:44:15 +0000

    Initial commit

---
+++ main.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+int age = 9;
+String name = 'Abdurrahmaan';
+var countris = ['zambia', 'south africa'];
+var map = {'name':'abdul','from':'zambia','going to':'south africa'};
+	print(name + " " + '$age' + " " + '$countris');
+  print(map);
+}
